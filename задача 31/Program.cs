b8d311f37ddedc2fe8236b4845f73db342a7b8ea
NastaBystritskaya <nasta.bystritskaya@gmail.com>
2023-07-02 01:16:34 +0300

    first commit

---
+++ задача 31/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
+// Найдите сумму отрицательных и положительных элементов массива. 
+
+int[] array = new int[12];
+int sum_positive = 0;
+int sum_negative = 0;
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++){
+    array[i] = rand.Next(-9, 10);
+}
+System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+for (int i = 0; i < array.Length; i++){
+    if (array[i]>0) {
+        sum_positive += array[i];
+    }
+    else {
+        sum_negative += array[i];
+    }
+}
+System.Console.WriteLine($"Сумма положительных элементов массива: {sum_positive}");
+System.Console.WriteLine($"Сумма отрицательных элементов массива: {sum_negative}");
---
+++ задача 32/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу замена элементов массива: положительные замените на соответствующие отрицательные и наоборот. 
+
+int[] GetArray() {
+    int[] Massive = new int[4];
+    for (int i = 0; i < Massive.Length; i++) {
+        Massive[i] = new Random().Next(-99, 100);
+    }
+    return Massive;
+} 
+
+int[] Swap(int[] Array) {
+    for (int i = 0; i < Array.Length; i++) {
+        Array[i] =  Array[i] * (-1);
+    }
+    return Array;
+}
+
+void Main() {
+    int[] Array =  GetArray();
+    System.Console.WriteLine(String.Join(", ", Array));
+
+    int[] Swaped = Swap(Array);
+    System.Console.WriteLine(String.Join(", " , Swaped));
+}
+
+Main();
